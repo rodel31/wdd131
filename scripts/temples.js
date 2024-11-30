8b49c5b7880cb6533eb5a803b06e6f4c2c7621af
Rodel Ramos <84778672+rodel31@users.noreply.github.com>
2024-11-30 21:32:16 +0800

    Do W02 Assignment: Picture Album

---
+++ scripts/temples.js
@@ -0,0 +1,9 @@
+// Hamburger Menu Toggle
+document.getElementById('hamburger').addEventListener('click', () => {
+    const menu = document.getElementById('menu');
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+});
+
+// Footer Date
+document.getElementById('year').textContent = new Date().getFullYear();
+document.getElementById('last-modified').textContent = document.lastModified;
